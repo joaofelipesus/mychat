1251bceeeebe18941308c57b88f362e9d647b93f
joaofelipe <joaofelipe1294@gmail.com>
2020-06-17 20:04:31 -0300

    Add registrations_controller to work with custom params;

--- app/controllers/team_controller.rb
+++ app/controllers/team_controller.rb
@@ -1,7 +1,6 @@
 class TeamController < ApplicationController
+
   def index
   end
 
-  def show
-  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ApplicationRecord
   validates_presence_of :name
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
+  has_one_attached :avatar
 end
