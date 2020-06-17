679fd524f607474dffb4e8c5cc97e5afda120dd9
joaofelipe <joaofelipe1294@gmail.com>
2020-06-17 10:58:38 -0300

    Generated user specs.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  validates_presence_of :name
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
 end
