0c1ae092880399f3fcca342660c2a16e92da88a9
joaofelipe <joaofelipe1294@gmail.com>
2020-06-17 21:14:07 -0300

    Fixed modeling error. All functionalities migrated from grouop_users to team_users.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_one_attached :avatar
+  has_many :team_users
   has_many :teams, through: :team_users
 
-  def my_groups
+  def my_teams
     Team.where(owner: self) + self.teams
   end
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe User, type: :model do
     end
   end
 
-  describe 'my_groups' do
+  describe 'my_teams' do
     context 'when user is owner of a group' do
       before :each do
         @user = create(:user)
@@ -24,7 +24,7 @@ RSpec.describe User, type: :model do
         create(:user)
         create(:team)
         @user = create(:user)
-        create(:group_user, user: @user)
+        create(:team_user, user: @user)
       end
       it 'is expected to return 1 group' do
         expect(@user.reload.my_teams.size).to eq 1
@@ -33,16 +33,15 @@ RSpec.describe User, type: :model do
     context 'when user is owner of a group and member of other group' do
       before :each do
         @user = create(:user)
-        create(:team, owner: @user)
+        create(:team, owner: @user, slug: 'nerv')
         other_user = create(:user)
-        create(:team, owner: other_user)
-        create(:group_user, user: @user)
+        create(:team, owner: other_user, slug: 'evangelion')
+        create(:team_user, user: @user)
       end
       it 'is expected to return 2 groups' do
         expect(@user.reload.my_teams.size).to eq 2
       end
     end
-
   end
 
 end
