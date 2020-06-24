ef6744a61c3f58641c93bf08766423d02ccdb880
joaofelipe <joaofelipe1294@gmail.com>
2020-06-23 22:45:33 -0300

    Generated mailer, and email sent when user is invited to user

--- app/models/team_user.rb
+++ app/models/team_user.rb
@@ -2,6 +2,7 @@ class TeamUser < ApplicationRecord
   belongs_to :team
   belongs_to :user
   before_validation :set_inviting_status
+  after_create :send_confirmation_mail
   validates :team, uniqueness: { scope: :user }
   enum inviting_status: {
     pending: 0,
@@ -12,5 +13,9 @@ class TeamUser < ApplicationRecord
 
     def set_inviting_status
       self.inviting_status = :pending unless self.inviting_status
+    end
+
+    def send_confirmation_mail
+      TeamUserMailer.new_invite(self).deliver_now!
     end
 end
