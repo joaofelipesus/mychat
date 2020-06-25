b6f53c8e791ef19e6f1e1ea533fbc66ced3bc14a
joaofelipe <joaofelipe1294@gmail.com>
2020-06-25 14:43:03 -0300

    Added form used to render the chat.

---
+++ app/javascript/packs/teams/show.js
@@ -0,0 +1,19 @@
+$(document).on('turbolinks:load', () => {
+
+  alert('Create message_controller !!!')
+
+  
+
+
+
+
+
+})
+
+
+// Send form with enter press
+// $('#id_of_textarea').keypress(function(e){
+//       if(e.which == 13){
+//            $(this).closest('form').submit();
+//        }
+//     });
