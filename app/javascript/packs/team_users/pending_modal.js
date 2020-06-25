36fbec6d94442fc836b606a053c869bf34aa587d
joaofelipe <joaofelipe1294@gmail.com>
2020-06-25 10:19:13 -0300

    Added decline team invite.

--- app/javascript/packs/team_users/pending_modal.js
+++ app/javascript/packs/team_users/pending_modal.js
@@ -22,11 +22,28 @@ $(document).on("turbolinks:load", () => {
       data: { team_user: { inviting_status: "confirmed" } },
       dataType: 'json',
       success: response => {
-        M.toast({ html: 'Invite successful accepted', classes: 'green' })
+        M.toast({ html: 'Invite accepted', classes: 'green' })
         $('#new-invite-modal').modal('close')
       },
       error: response => {
         M.toast({ html: response["errors"][0], classes: 'red' })
+      }
+    })
+  })
+
+  $('#decline-invite').on('click', event => {
+    const team_user_id = $(event.target).attr('data-value')
+    $.ajax({
+      url: `/team_users/${team_user_id}`,
+      method: 'DELETE',
+      dataType: 'json',
+      success: response => {
+        M.toast({ html: 'Inite declined', classes: 'green'})
+        $('#new-invite-modal').modal('close')
+      },
+      error: response => {
+        const error_message = response["errors"][0]
+        M.toast({ html: error_message, classes: 'red' })
       }
     })
   })
