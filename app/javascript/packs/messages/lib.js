21bde4f8a8542996044596cc8749c70bd7ef74f6
joaofelipe <joaofelipe1294@gmail.com>
2020-07-14 16:01:24 -0300

    Added sender name on the side of each message, also set a distinct color to each user.

--- app/javascript/packs/messages/lib.js
+++ app/javascript/packs/messages/lib.js
@@ -1,3 +1,5 @@
+const COLORS = ['blue-text', 'deep-purple-text darken-1', 'teal-text darken-3', 'brown-text darken-1']
+
 const Message = {
   send() {
     const body = $('#message_body').val()
@@ -25,10 +27,34 @@ const Message = {
     const current_user = $('#current-user').text()
     let message_element = null
     if (current_user == message.user_id)
-      message_element = `<div class='message right-align'>${message.body}</div>`
+      message_element = `
+        <div class='message right-align'>
+          ${message.body}
+        </div>`
     else
-      message_element = `<div class="message">${message.body}</div>`
+      message_element = `
+        <div class="message">
+          <span class='login-${message.user_id}'>${message.user.name}:</span>
+          ${message.body}
+        </div>`
     $('#chat').append(message_element)
+    let login_color_relation = $('#login-color-relation')
+    if(login_color_relation.length > 0){
+      let relation = JSON.parse($('#login-color-relation').text())
+      if (`login-${message.user_id}` in relation){
+        $(`.login-${message.user_id}`).addClass(relation[`login-${message.user_id}`])
+      }else{
+        relation[`login-${message.user_id}`] = COLORS[Object.keys(relation).length]
+        // $(`.login-${message.user_id}`).addClass(COLORS[Object.keys(relation).length])
+        $('#login-color-relation').text(JSON.stringify(relation))
+      }
+    }else{
+      $('body').append(`<span id='login-color-relation'></span>`)
+      let relation = {}
+      relation[`login-${message.user_id}`] = COLORS[0]
+      $('#login-color-relation').text(JSON.stringify(relation))
+      $(`.login-${message.user_id}`).addClass(COLORS[0])
+    }
   },
 
   load(){
@@ -39,6 +65,7 @@ const Message = {
       dataType: 'json',
       success: response => {
         const messages = response["messages"]
+        console.log(messages);
         messages.forEach(message => Message.add(message))
       },
       error: response => {
@@ -49,56 +76,3 @@ const Message = {
 };
 
 export default Message;
-
-
-
-
-
-
-
-
-
-
-
-
-//
-// function send_message(){
-//   const body = $('#message_body').val()
-//   const current_group = localStorage.getItem('current-group')
-//   const message = {
-//     body: body,
-//     group_id: current_group
-//   }
-//   $.ajax({
-//     url: '/messages',
-//     data: { message: message },
-//     method: 'POST',
-//     success: response => {
-//       $('#message_body').val('')
-//       // TODO: Load message to chat ...
-//     },
-//     error: response => {
-//       M.toast({ html: response["errors"][0], classes: 'red' })
-//     }
-//   })
-// }
-
-// function add_message(message){
-//   console.log(message);
-// }
-
-// function load_messages(){
-//   const current_group = localStorage.getItem('current-group')
-//   $.ajax({
-//     url: `/messages?group=${current_group}`,
-//     method: 'GET',
-//     dataType: 'json',
-//     success: response => {
-//       const messages = response["messages"]
-//       messages.forEach(message => add_message(message))
-//     },
-//     error: response => {
-//       M.toast({ html: response["errors"][0], classes: 'red' })
-//     }
-//   })
-// }
